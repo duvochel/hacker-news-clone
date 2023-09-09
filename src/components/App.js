7c7028865f7fef113bd3f80091353a2cbbeb6a95
Francois <francois@devfullstack.com>
2023-09-09 15:37:24 +0200

    feat: create link

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
-import LinkList from './LinkList';
+import React, { Component } from "react";
+import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
 class App extends Component {
   render() {
-    return <LinkList />;
+    return (
+      <div className="mw5 mw7-ns center pa3 ph5-ns">
+        <LinkList />
+        <CreateLink />
+      </div>
+    );
   }
 }
 
